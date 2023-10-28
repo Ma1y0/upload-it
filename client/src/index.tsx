ce5fa144cd1a336e3bb6422e3669c6d2447ef580
Ma1y0 <matyas.barr@gmail.com>
2023-10-28 16:31:02 +0200

    JWT backend

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,21 @@
 /* @refresh reload */
-import { render } from 'solid-js/web'
+import { render } from "solid-js/web";
+import { Router, Route, Routes } from "@solidjs/router";
 
-import './index.css'
-import App from './App'
+import "./index.css";
+import App from "./App";
+import NavBar from "./components/NavBar";
 
-const root = document.getElementById('root')
+const root = document.getElementById("root");
 
-render(() => <App />, root!)
+render(
+  () => (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" component={App} />
+      </Routes>
+    </Router>
+  ),
+  root!,
+);
